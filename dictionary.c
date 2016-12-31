8325a39d7f4681f24a695f31b015fc4ac278c6b8
Aakash Garg <aakash.garg80@gmail.com>
2016-12-31 08:43:07 +0000

    implemented load function

--- dictionary.c
+++ dictionary.c
@@ -7,9 +7,35 @@
  * Implements a dictionary's functionality.
  */
 
-#include <stdbool.h>
+#include<stdbool.h>
+#include<stddef.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
 
 #include "dictionary.h"
+
+typedef struct node
+{
+    char word[LENGTH+1];
+    struct node* next;    
+}
+node;
+
+node* HashTable[SIZE] = {0};
+
+//hash function---source: reddit.com
+int hash(char* needs_hashing)
+{
+    unsigned int hash_num = 0;
+    for (int i=0, n=strlen(needs_hashing); i<n; i++)
+        hash_num = (hash << 2) ^ needs_hashing[i];
+    return hash_num % SIZE;
+}
+
+
+
 
 /**
  * Returns true if word is in dictionary else false.
@@ -25,8 +51,40 @@ bool check(const char* word)
  */
 bool load(const char* dictionary)
 {
-    // TODO
-    return false;
+    FILE* fp;
+    fp=fopen(dictionary,"r");
+    if(fp==NULL)
+    {
+        return false;
+    }
+    
+    while(!feof(fp))
+    {
+        node* new_node = malloc(sizeof(node));
+        if(new_node==NULL)
+        {
+            printf("memory not allocated\n");
+            return false;
+        }
+        
+        fscanf(fp,"%s",new_node->word);
+        new_node->next=NULL;
+        
+        int h = hash(new_node->word);
+        node* head = HashTable[h];
+        if(head==NULL)
+        {
+            HashTable[h]=new_node;
+        }
+        else
+        {
+            new_node->next = HashTable[h];
+            HashTable[h]=new_node;
+        }
+    }
+    fclose(fp);
+    return true;
+    
 }
 
 /**
